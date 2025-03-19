98e3b64494023f0f4f89bffa40be0e10b3ea619f
Bruno Eduardo Maruno <bruno_maruno@hotmail.com>
2025-03-19 16:11:54 -0300

    Insert config to cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
+  projectID: 'qnvxs2',
   e2e: {
   }/* ,
   video: true */
